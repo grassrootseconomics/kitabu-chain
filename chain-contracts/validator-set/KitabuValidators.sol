108349fd33df5919e30bc415d570a2b71ee82a4c
Mohammed Sohail <sohailsameja@gmail.com>
2022-04-04 09:47:02 +0300

    update: block time to 6s, bootstrap validator set

--- chain-contracts/validator-set/KitabuValidators.sol
+++ chain-contracts/validator-set/KitabuValidators.sol
@@ -1,16 +1,20 @@
 // Author: Mohamed Sohail <mohamedsohailazim@gmail.com>
 // SPDX-License-Identifier:	GPL-3.0-or-later
 
-pragma solidity >= 0.6.12;
+pragma solidity >=0.6.12;
 
 contract KitabuValidators {
-    address constant SYSTEM_ADDRESS = 0xffffFFFfFFffffffffffffffFfFFFfffFFFfFFfE;
-    address public admissionController = 0x289DeFD53E2D96F05Ba29EbBebD9806C94d04Cb6;
+    address constant SYSTEM_ADDRESS =
+        0xffffFFFfFFffffffffffffffFfFFFfffFFFfFFfE;
+    address public admissionController =
+        0x289DeFD53E2D96F05Ba29EbBebD9806C94d04Cb6;
 
     address[] bootstrapValidators = [
         0x8bEDFA3Af524911b9E57b33D3982c2b73D1e20e1,
         0xb0dCcC83B5AF0F36Bd525a1549eec27928D802cc,
-        0xA26049EDdC0EABA16b010ec509Db6046AE56B0CD
+        0xA26049EDdC0EABA16b010ec509Db6046AE56B0CD,
+        0x3F78b6CB6Cb82f513A515Ff0222810F7aB6F56d5,
+        0xEB701b8cB5F2d040426Eb16E39710083B9B19e1C
     ];
 
     address[] validators;
@@ -18,70 +22,83 @@ contract KitabuValidators {
     bool public finalized;
 
     mapping(address => uint256) validatorSetIndex;
- 
-    event NewAdmissionController(address indexed old, address indexed newController);
+
+    event NewAdmissionController(
+        address indexed old,
+        address indexed newController
+    );
     event InitiateChange(bytes32 indexed parentHash, address[] newSet);
     event ChangeFinalized(address[] currentSet);
 
-    modifier onlySystemChange {
+    modifier onlySystemChange() {
         require(msg.sender == SYSTEM_ADDRESS);
         _;
-    }    
+    }
 
-    modifier alreadyFinalized {
+    modifier alreadyFinalized() {
         require(finalized);
         _;
-    }    
+    }
 
-    modifier onlyAdmissionController {
+    modifier onlyAdmissionController() {
         require(msg.sender == admissionController);
         _;
-    }    
+    }
 
     constructor() public {
-        for (uint i = 0; i < bootstrapValidators.length; i++) {
+        for (uint256 i = 0; i < bootstrapValidators.length; i++) {
             validators.push(bootstrapValidators[i]);
-		    validatorSetIndex[bootstrapValidators[i]] = i; 
+            validatorSetIndex[bootstrapValidators[i]] = i;
         }
 
         finalized = true;
     }
 
-    function setNewAdmissionController(address newController) public onlyAdmissionController {
+    function setNewAdmissionController(address newController)
+        public
+        onlyAdmissionController
+    {
         emit NewAdmissionController(admissionController, newController);
         admissionController = newController;
     }
 
-    function getValidators() public view returns(address[] memory) {
-		return validators;
-	}
+    function getValidators() public view returns (address[] memory) {
+        return validators;
+    }
 
     function initiateChange() private {
-		finalized = false;
-		emit InitiateChange(blockhash(block.number - 1), getValidators());
-	}
+        finalized = false;
+        emit InitiateChange(blockhash(block.number - 1), getValidators());
+    }
 
     function finalizeChange() public {
         finalized = true;
         emit ChangeFinalized(validators);
     }
 
-
-	function addValidator(address newValidator) public onlyAdmissionController alreadyFinalized{
+    function addValidator(address newValidator)
+        public
+        onlyAdmissionController
+        alreadyFinalized
+    {
         validators.push(newValidator);
         validatorSetIndex[newValidator] = validators.length - 1;
-		initiateChange();
-	}
+        initiateChange();
+    }
 
-	function removeValidator(address exValidator) public onlyAdmissionController alreadyFinalized{
+    function removeValidator(address exValidator)
+        public
+        onlyAdmissionController
+        alreadyFinalized
+    {
         orderedRemoval(validatorSetIndex[exValidator]);
         delete validatorSetIndex[exValidator];
         initiateChange();
-	}
+    }
 
-    function orderedRemoval(uint index) private {
-        for(uint i = index; i < validators.length-1; i++){
-            validators[i] = validators[i+1];      
+    function orderedRemoval(uint256 index) private {
+        for (uint256 i = index; i < validators.length - 1; i++) {
+            validators[i] = validators[i + 1];
         }
         validators.pop();
     }
